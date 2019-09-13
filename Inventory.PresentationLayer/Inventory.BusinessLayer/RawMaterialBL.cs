b67abecb419d23ecd6b3e318da005758ee71c7ef
RohitKumarCG <54537801+RohitKumarCG@users.noreply.github.com>
2019-09-14 02:41:02 +0530

    Add files via upload

---
+++ Inventory.PresentationLayer/Inventory.BusinessLayer/RawMaterialBL.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Entities;
+using Inventory.DataAccessLayer;
+using System.Text.RegularExpressions;
+using Inventory.Exception;
+
+namespace Inventory.BusinessLayer
+{
+    public class RawMaterialBL
+    {
+        private static bool ValidateRawMaterial(RawMaterial rawMaterial)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool validRawMaterial = true;
+            if (rawMaterial.RawMaterialID == String.Empty || rawMaterial.RawMaterialID.Length > 5)
+            {
+                validRawMaterial = false;
+                sb.Append("\nInvalid Raw Material ID");
+            }
+            Regex regex1 = new Regex("^[a-zA-Z]+$");
+            if (!regex1.IsMatch(rawMaterial.RawMaterialName) || rawMaterial.RawMaterialName == String.Empty || rawMaterial.RawMaterialName.Length > 30)
+            {
+                validRawMaterial = false;
+                sb.Append("\nInvalid Raw Material Name");
+            }
+            Regex regex2 = new Regex("^[a-zA-Z]+$");
+            if (!regex2.IsMatch(rawMaterial.RawMaterialName) || rawMaterial.RawMaterialCode == String.Empty || rawMaterial.RawMaterialCode.Length > 4)
+            {
+                validRawMaterial = false;
+                sb.Append("\nInvalid Raw Material Code");
+            }
+            if (validRawMaterial == false)
+            {
+                throw new InventoryException(sb.ToString());
+            }
+            return validRawMaterial;
+        }
+
+        public static bool AddRawMaterialBL(RawMaterial newRawMaterial)
+        {
+            bool rawMaterialAdded = false;
+            try
+            {
+                if (ValidateRawMaterial(newRawMaterial))
+                {
+                    RawMaterialDAL rawMaterialDAL = new RawMaterialDAL();
+                    rawMaterialAdded = rawMaterialDAL.AddRawMaterialDAL(newRawMaterial);
+                }
+            }
+            catch (InventoryException)
+            {
+                throw;
+            }
+            catch (SystemException ex)
+            {
+                throw ex;
+            }
+
+            return rawMaterialAdded;
+        }
+
+        public static bool DeleteRawMaterialBL(string deleteRawMaterialID)
+        {
+            bool rawMaterialDeleted = false;
+            try
+            {
+                if(deleteRawMaterialID.Length > 0 && deleteRawMaterialID.Length < 5)
+                {
+                    RawMaterialDAL rawMaterialDAL = new RawMaterialDAL();
+                    rawMaterialDeleted = rawMaterialDAL.DeleteRawMaterialDAL(deleteRawMaterialID);
+                }
+                else
+                {
+                    throw new InventoryException("Invalid Raw Material ID");
+                }
+
+            }
+            catch(InventoryException ex)
+            {
+                throw;
+            }
+            catch (SystemException ex)
+            {
+                throw ex;
+            }
+            return rawMaterialDeleted;
+        }
+
+        public static List<RawMaterial> GetAllRawMaterialsBL()
+        {
+            List<RawMaterial> rawMaterialList = null;
+            try
+            {
+                RawMaterialDAL rawMaterialDAL = new RawMaterialDAL();
+                rawMaterialList = rawMaterialDAL.GetAllRawMaterialsDAL();
+            }
+            catch (InventoryException ex)
+            {
+                throw ex;
+            }
+            catch (SystemException ex)
+            {
+                throw ex;
+            }
+            return rawMaterialList;
+        }
+
+        public static RawMaterial SearchRawMaterialByIDBL(string searchRawMaterialID)
+        {
+            RawMaterial searchRawMaterial = null;
+            try
+            {
+                RawMaterialDAL rawMaterialDAL = new RawMaterialDAL();
+                searchRawMaterial = rawMaterialDAL.SearchRawMaterialByIDDAL(searchRawMaterialID);
+            }
+            catch (InventoryException ex)
+            {
+                throw ex;
+            }
+            catch (SystemException ex)
+            {
+                throw ex;
+            }
+            return searchRawMaterial;
+        }
+
+        public static RawMaterial SearchRawMaterialByNameBL(string searchRawMaterialName)
+        {
+            RawMaterial searchRawMaterial = null;
+            try
+            {
+                RawMaterialDAL rawMaterialDAL = new RawMaterialDAL();
+                searchRawMaterial = rawMaterialDAL.SearchRawMaterialByNameDAL(searchRawMaterialName);
+            }
+            catch (InventoryException ex)
+            {
+                throw ex;
+            }
+            catch (SystemException ex)
+            {
+                throw ex;
+            }
+            return searchRawMaterial;
+        }
+
+        public static RawMaterial SearchRawMaterialByCodeBL(string searchRawMaterialCode)
+        {
+            RawMaterial searchRawMaterial = null;
+            try
+            {
+                RawMaterialDAL rawMaterialDAL = new RawMaterialDAL();
+                searchRawMaterial = rawMaterialDAL.SearchRawMaterialByCodeDAL(searchRawMaterialCode);
+            }
+            catch (InventoryException ex)
+            {
+                throw ex;
+            }
+            catch (SystemException ex)
+            {
+                throw ex;
+            }
+            return searchRawMaterial;
+        }
+    }
+}
---
+++ Inventory.PresentationLayer/Inventory.DataAccessLayer/RawMaterialDAL.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Entities;
+using Inventory.Exception;
+
+namespace Inventory.DataAccessLayer
+{
+    public class RawMaterialDAL
+    {
+        public static List<RawMaterial> rawMaterialList = new List<RawMaterial>();
+
+        public bool AddRawMaterialDAL(RawMaterial newRawMaterial)
+        {
+            bool rawMaterialAdded = false;
+            try
+            {
+                rawMaterialList.Add(newRawMaterial);
+                rawMaterialAdded = true;
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return rawMaterialAdded;
+        }
+
+        public bool DeleteRawMaterialDAL(string deleteRawMaterialID)
+        {
+            bool rawMaterialDeleted = false;
+            try
+            {
+                RawMaterial deleteRawMaterial = null;
+                foreach (RawMaterial item in rawMaterialList)
+                {
+                    if (item.RawMaterialID == deleteRawMaterialID)
+                    {
+                        deleteRawMaterial = item;
+                    }
+                }
+
+                if (deleteRawMaterial != null)
+                {
+                    rawMaterialList.Remove(deleteRawMaterial);
+                    rawMaterialDeleted = true;
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return rawMaterialDeleted;
+
+        }
+
+        public List<RawMaterial> GetAllRawMaterialsDAL()
+        {
+            return rawMaterialList;
+        }
+
+        public RawMaterial SearchRawMaterialByIDDAL(string searchRawMaterialID)
+        {
+            RawMaterial searchRawMaterial = null;
+            try
+            {
+                foreach (RawMaterial item in rawMaterialList)
+                {
+                    if (item.RawMaterialID == searchRawMaterialID)
+                    {
+                        searchRawMaterial = item;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return searchRawMaterial;
+        }
+
+        public RawMaterial SearchRawMaterialByNameDAL(string searchRawMaterialName)
+        {
+            RawMaterial searchRawMaterial = null;
+            try
+            {
+                foreach (RawMaterial item in rawMaterialList)
+                {
+                    if (item.RawMaterialName == searchRawMaterialName)
+                    {
+                        searchRawMaterial = item;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return searchRawMaterial;
+        }
+
+        public RawMaterial SearchRawMaterialByCodeDAL(string searchRawMaterialCode)
+        {
+            RawMaterial searchRawMaterial = null;
+            try
+            {
+                foreach (RawMaterial item in rawMaterialList)
+                {
+                    if (item.RawMaterialCode == searchRawMaterialCode)
+                    {
+                        searchRawMaterial = item;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return searchRawMaterial;
+        }  
+    }
+}
